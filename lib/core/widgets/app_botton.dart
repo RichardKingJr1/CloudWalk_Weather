b6d5f9618f9434c6212bf40697290cf9a76996e1
Richard King <richard.kingjunior1@gmail.com>
2023-07-22 18:28:20 -0300

    feature: add page current weather

---
+++ lib/core/widgets/app_botton.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+class AppBotton extends StatefulWidget {
+  var cont;
+  int selectedIndex;
+
+  AppBotton({Key? key, required this.cont, required this.selectedIndex})
+      : super(key: key);
+
+  @override
+  _AppBottonState createState() => _AppBottonState();
+}
+
+class _AppBottonState extends State<AppBotton> {
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      showSelectedLabels: false,
+      showUnselectedLabels: false,
+      currentIndex: widget.selectedIndex,
+      items: [
+        const BottomNavigationBarItem(
+            icon: Icon(Icons.home), label: '' ),
+        const BottomNavigationBarItem(
+            icon: Icon(Icons.abc), label: '' ),
+      ],
+      unselectedItemColor: Colors.grey,
+      selectedItemColor: Colors.black,
+      onTap: onTap,
+    );
+  }
+
+  void onTap(int index) {
+    setState(() {
+      switch (index) {
+        case 0:
+          Navigator.of(widget.cont)
+              .pushNamedAndRemoveUntil('/current', (Route<dynamic> route) => false);
+          break;
+        case 1:
+          Navigator.of(widget.cont)
+              .pushNamedAndRemoveUntil('/forecast', (Route<dynamic> route) => false);
+          break;
+      }
+    });
+  }
+}
--- lib/modules/weather/presenter/pages/current_weather_page.dart
+++ lib/modules/weather/presenter/pages/current_weather_page.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+
+import '../../../../core/widgets/app_botton.dart';
 
 class CurrentWeatPage extends StatefulWidget {
   const CurrentWeatPage({super.key});
@@ -11,6 +14,59 @@ class CurrentWeatPage extends StatefulWidget {
 class _CurrentWeatPageState extends State<CurrentWeatPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      bottomNavigationBar: AppBotton(cont: context, selectedIndex: 0),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10),
+            child: Column(
+              children: [
+                Container(
+                  height: 170,
+                  child: Card(
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.only(top: 10,left: 20.0),
+                            child: Text(
+                              'Cidade',
+                              style: const TextStyle(color: Colors.black, fontSize: 20, fontWeight: FontWeight.w400),
+                            ),
+                          ),
+                          SizedBox(height: 5),
+                          Row(
+                            children: [
+                              Image.network('http://openweathermap.org/img/wn/10d@2x.png'),
+                              SizedBox(width: 5),
+                              Text(
+                                '60°',
+                                style: const TextStyle(color: Colors.black, fontSize: 40, fontWeight: FontWeight.w400),
+                              ),
+                              Spacer(),
+                              Column(
+                                children: [
+                                  Text('Chuva'),
+                                  Text('min: 14°'),
+                                  Text('max: 20°'),
+                                ],
+                              ),
+                              SizedBox(width: 20,)
+                            ],
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
